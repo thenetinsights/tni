9020ccce4afa0bb344f5e587fb96eda5387dc2b7
thenetinsights <cliffjonye36@gmail.com>
2024-03-13 22:54:59 +0300

    lnw

---
+++ index.js
@@ -0,0 +1,98 @@
+window.onload = fucntion() {
+    window.addEventListener('scroll', function(e) {
+            if (window.pageXOffset > 100) {
+                document.querySelector("header").classList.add('is-scrolling');
+            } else {
+                document.querySelector("header").classList.remove('is-scrolling');
+            }
+        }
+    });
+const menu_btn = document.querySelector('hamburger');
+menu menu_btn.addEventListener('click', function() {
+    menu_btn.classList.add('is-active');
+
+}
+});
+}
+
+
+
+function toggleNav() {
+    const nav = document.getElementById("navbar");
+    nav.style.display = mav.style.display == "none" ? "block" : "none";
+}
+
+/*homepage slideshow******************************/
+document.addEventListener("DOMContentLoaded", function() {
+    const sliderWrapper = document.querySelector(".slider-wrapper");
+    const slider = document.querySelector(".slider");
+    let isDragging = false;
+    let startPositionX = 0;
+    let currentTranslateX = 0;
+    let prevTranslateX = 0;
+    let animationId;
+
+    slider.addEventListener("touchstart", (e) => {
+        isDragging = true;
+        startPositionX = e.touches[0].clientX;
+        prevTranslateX = currentTranslateX;
+        cancelAnimationFrame(animationId);
+    });
+
+    slider.addEventListener("touchend", () => {
+        isDragging = false;
+        const movedBy = currentTranslateX - prevTranslateX;
+        if (movedBy < -100 && canMoveRight()) {
+            moveRight();
+        } else if (movedBy > 100 && canMoveLeft()) {
+            moveLeft();
+        } else {
+            cancelMove();
+        }
+    });
+
+    slider.addEventListener("touchmove", (e) => {
+        if (isDragging) {
+            const currentPositionX = e.touches[0].clientX;
+            currentTranslateX = prevTranslateX + currentPositionX - startPositionX;
+            updateSliderPosition();
+        }
+    });
+
+    slider.addEventListener("touchcancel", () => {
+        isDragging = false;
+        cancelMove();
+    });
+
+    function updateSliderPosition() {
+        slider.style.transform = `translateX(${currentTranslateX}px)`;
+    }
+
+    function canMoveLeft() {
+        return currentTranslateX < 0;
+    }
+
+    function canMoveRight() {
+        const sliderWidth = slider.getBoundingClientRect().width;
+        const slideWidth = sliderWrapper.getBoundingClientRect().width;
+        return currentTranslateX > -(sliderWidth - slideWidth);
+    }
+
+    function moveLeft() {
+        currentTranslateX += sliderWrapper.getBoundingClientRect().width;
+        updateSliderPosition();
+    }
+
+    function moveRight() {
+        currentTranslateX -= sliderWrapper.getBoundingClientRect().width;
+        updateSliderPosition();
+    }
+
+    function cancelMove() {
+        currentTranslateX = prevTranslateX;
+        updateSliderPosition();
+    }
+});
+
+
+//-----------ourTeam Flip Cards-----///
